1852b0020bb6e165ba476290e2364f78b3d11734
Jambo1803 <shreyathorat38@gmail.com>
2026-08-08 11:44:03 +0530

    Update BranchPractice on main

--- Arrays/BranchPractice.java
+++ Arrays/BranchPractice.java
@@ -1,6 +1,6 @@
 public class BranchPractice {
 
     public static void main(String[] args) {
-        System.out.println("I am practicing Git branches!");
+        System.out.println("This is the MAIN branch");
     }
 }
